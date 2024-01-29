d638dbfa98a3e4b625dd74fdf09aed307741bf82
Lactovacilo <jpe.bgi@gmail.com>
2024-01-28 21:17:28 -0300

    2801Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Container} from 'react-bootstrap'
 
 import MainMenu from "./components/MainMenu.jsx";
 import Game from "./components/Game.jsx";
@@ -9,12 +9,14 @@ import Game from "./components/Game.jsx";
 
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<MainMenu />} />
-              <Route path="/Game/:nPares" element={<Game />} />
-          </Routes>
-      </BrowserRouter>
+      <Container>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<MainMenu />} />
+                  <Route path="/Game/:nPares" element={<Game />} />
+              </Routes>
+          </BrowserRouter>
+      </Container>
   );
 }
 
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,68 +1,130 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import ReactCardFlip from "react-card-flip";
 import {useParams} from "react-router-dom";
-import {Col, Container, Row, Image} from "react-bootstrap";
+import {Col, Row, Image} from "react-bootstrap";
 
 
 function Game() {
     const params = useParams()
-    const [cardsFlipped, setCardsFlipped] = useState([])
+
+    const [visualCards, setVisualCards] = useState([])
     const [cards, setCards] = useState([])
-    const [firstCard, setFirstCard] = useState(null)
 
-    function handleCardClick(event) {
-        console.log("Clicked", event.target.parentNode.parentNode.parentNode.parentNode)
+    const firstCard = useRef(-1)
 
-        if (firstCard == null) {
-            console.log("First card clicked")
-        } else {
-            console.log("Second card clicked")
-        }
-    }
+    const clickable = useRef(true)
 
-    function generateCards() {
-        let localFlippedCards = []
-        let localCards = []
+    const cardsSolved = useRef(0)
 
-        for (let i = 0; i < params.nPares; i++) {
-            let randomIndex1 = Math.floor(Math.random() * localCards.length)
-            let card1 = createCard(i, 2 * i)
-            localFlippedCards.splice(randomIndex1, 0, false)
-            localCards.splice(randomIndex1, 0, card1)
 
-            let randomIndex2 = Math.floor(Math.random() * localCards.length)
-            let card2 = createCard(i, 2 * i + 1)
-            localFlippedCards.splice(randomIndex2, 0, false)
-            localCards.splice(randomIndex2, 0, card2)
-        }
-
-        setCardsFlipped(localFlippedCards)
-        setCards(localCards)
-
-        console.log(localCards, localFlippedCards)
-    }
-
-    function createCard(cardId, key) {
+    function createCard(index) {
         return (
-            <Col key={key} data-id={cardId} >
-                <ReactCardFlip isFlipped={false}>
-                    <Image style={{cursor: "pointer"}} onClick={(event) => handleCardClick(event)} src="/img/emptyRune.png" />
-                    <Image src={"/img/cards/runa"+cardId+".png"} />
+            <Col key={cards[index].key} >
+                <ReactCardFlip isFlipped={cards[index].flipped}>
+                    <Image onClick={() => handleCardClick(cards[index].id, cards[index].key)} src="/img/emptyRune.png" style={{cursor: "pointer"}} />
+                    <Image src={"/img/cards/runa"+cards[index].id+".png"} />
                 </ReactCardFlip>
             </Col>
         )
     }
 
+    // Função para virar um card, seja para cima ou para baixo
+    function turnCard(key, bool) {
+        let localCards = []
+        cards.forEach((item) => {
+            let itemCopy = Object.assign({}, item)
+            if (itemCopy.key === key) {
+                itemCopy.flipped = bool
+            }
+            localCards.push(itemCopy)
+        })
+
+        return localCards
+    }
+
+    // Função que seja executada ao clicar no card
+    function handleCardClick(cardId, key) {
+        if (clickable.current) {
+            let reversedCard = turnCard(key, true)
+            setCards(reversedCard)
+
+            if (firstCard.current === -1) {
+                firstCard.current = key
+
+            } else {
+                clickable.current = false
+
+                // forEach é utilizado para achar o id do card com essa key
+                cards.forEach((item) => {
+                    if (item.key === firstCard.current) {
+                        // Se o ID do card que já está selecionado for diferente ao ID do card clicado, virar os dois
+                        if (!(item.id === cardId)) {
+                            setTimeout(() => {
+                                let listBothCardsReverted = turnCard(item.key, false)
+                                setCards(listBothCardsReverted)
+                                clickable.current = true
+                            }, 1000)
+
+                        } else {
+                            cardsSolved.current = cardsSolved.current + 1
+
+                            if (cardsSolved.current === parseInt(params.nPares)) {
+                                winGame()
+                            } else {
+                                clickable.current = true
+                            }
+                        }
+                    }
+                })
+                firstCard.current = -1
+
+            }
+        }
+    }
+
+    // Função para gerar cards aleatóriamente (ela não mostra os cards, apenas coloca um um id pra cada um
+    function generateCards() {
+        let localCards = []
+
+        for (let i = 0; i < parseInt(params.nPares); i++) {
+            let randomIndex1 = Math.round(Math.random() * localCards.length)
+            localCards.splice(randomIndex1, 0, {"flipped": false, "id": i, "key": 2 * i})
+
+            let randomIndex2 = Math.round(Math.random() * localCards.length)
+            localCards.splice(randomIndex2, 0, {"flipped": false, "id": i, "key": 2 * i + 1})
+        }
+        setCards(localCards)
+    }
+
+    // Função para atualizar as cartas que aparecem para o jogador
+    function updateGame() {
+        let localVisualCards = []
+
+        for (let i = 0; i < cards.length; i++) {
+            localVisualCards.push(createCard(i))
+        }
+        setVisualCards(localVisualCards)
+    }
+
+    // Irá rodar quando o jogador ganhar a partida
+    function winGame() {
+        console.log("Player won game")
+    }
+
+    // Roda apenas uma vez, quando a página carregar
     useEffect(() => {
         generateCards()
     }, []);
 
+    // Roda toda vez que houver alteração nos cards
+    useEffect(() => {
+        updateGame()
+    }, [cards]);
+
     return (
-        <Container>
-            <Row >
-                {cards}
-            </Row>
-        </Container>
+        <Row sm={parseInt(params.nPares)}>
+            {visualCards}
+        </Row>
     )
 }
 
